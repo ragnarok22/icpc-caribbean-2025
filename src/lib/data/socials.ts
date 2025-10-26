043f8f7010858529966d0a04c1fc966ce056cdd0
Reinier Hernández <sasuke.reinier@gmail.com>
2025-10-26 10:15:37 -0300

    fix(socials): differentiate telegram channel and group entries

--- src/lib/data/socials.ts
+++ src/lib/data/socials.ts
@@ -26,7 +26,7 @@ export const SOCIALS: Social[] = [
     },
   },
   {
-    name: "telegram",
+    name: "telegram-channel",
     url: "https://t.me/icpccaribbeanmedia",
     image: {
       logo: Telegram,
@@ -35,7 +35,7 @@ export const SOCIALS: Social[] = [
     },
   },
   {
-    name: "telegram",
+    name: "telegram-group",
     url: "https://t.me/+08XS4hp5GZJhNGMx",
     image: {
       logo: Telegram,
