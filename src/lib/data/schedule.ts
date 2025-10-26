24ad9b0613438812600762214221aaa6d4e97513
Reinier Hernández <sasuke.reinier@gmail.com>
2025-10-26 10:18:13 -0300

    fix(schedule): add timestamps to schedule dates

--- src/lib/data/schedule.ts
+++ src/lib/data/schedule.ts
@@ -1,21 +1,21 @@
 export const SCHEDULE = [
   {
-    date: "2025-11-06",
+    date: "2025-11-06T12:00:00",
     description:
       "Recibimiento, orientación, cena, inscripción oficial y XI Torneo Caribeño de Ajedrez (ICPChess).",
   },
   {
-    date: "2025-11-07",
+    date: "2025-11-07T12:00:00",
     description:
       "Ceremonia de inauguración, talleres, exposición de patrocinadores, concurso de práctica, torneos y actividades culturales.",
   },
   {
-    date: "2025-11-08",
+    date: "2025-11-08T12:00:00",
     description:
       "Concurso real con transmisión en vivo, conferencia de prensa, ceremonia de clausura y cena de despedida.",
   },
   {
-    date: "2025-11-09",
+    date: "2025-11-09T12:00:00",
     description: "Despedida de los equipos e invitados.",
   },
 ];
