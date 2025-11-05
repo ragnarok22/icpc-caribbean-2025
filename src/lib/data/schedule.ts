05f43f5400037d293b26b9662e64feac60ca73d3
Saul <sondonsaul@gmail.com>
2025-11-05 17:56:01 -0500

    fix: fix the date in the schedule
    Changed from nov 9 to november 8

--- src/lib/data/schedule.ts
+++ src/lib/data/schedule.ts
@@ -1,25 +1,16 @@
 export const SCHEDULE = [
   {
-    date: "2025-11-06T12:00:00",
+    date: "2025-11-06T20:00:00",
     description:
       "Recibimiento, orientación, cena, inscripción oficial y XI Torneo Caribeño de Ajedrez (ICPChess).",
   },
   {
-    date: "2025-11-07T12:00:00",
-    description:
-      "Ceremonia de inauguración, talleres, exposición de patrocinadores, torneos y actividades culturales.",
-  },
-  {
     date: "2025-11-08T09:00:00",
-    description: "Concurso de práctica.",
+    description: "Concurso de práctica (9am - 10am).",
   },
   {
-    date: "2025-11-09T12:00:00",
+    date: "2025-11-08T12:00:00",
     description:
-      "Concurso real con transmisión en vivo, conferencia de prensa, ceremonia de clausura y cena de despedida.",
-  },
-  {
-    date: "2025-11-09T12:00:00",
-    description: "Despedida de los equipos e invitados.",
+      "Concurso oficial (12pm - 5pm).",
   },
 ];
